50aebf3b344d09b901b6672b24dadb36e1d748eb
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-01-19 17:46:56 -0600

    More JavaScript accessing classes and rendering character.

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 // import functions
-
+import { people } from './data.js';
 // grab DOM elements
+const selectEl = document.querySelector('.select');
+const nameEl = document.querySelector('.name');
+const ageEl = document.querySelector('.age');
+const bioEl = document.querySelector('.bio');
+const hobbiesEl = document.querySelector('.hobbies');
+const container = document.querySelector('.profile');
+console.log(selectEl, nameEl, ageEl, bioEl, hobbiesEl, container);
 
 // set event listeners 
-    // get user input
-    // use user input to update state 
-    // update DOM to reflect the new state
+// get user input
+function userSelect{
+    container.classList.add('${people[index].name}');
+    nameEl.textContent = 'Name: ' + people[index].name;
+    ageEl.textContent = 'Name: ' + people[index].age;
+    bio.textContent = 'Name: ' + people[index].bio;
+
+    for (const hobby of people[index].hobbies) {
+        const li = document.createElement('li');
+        li.textContent = hobby;
+        hobbiesEl.appendChild(li);
+    }
+}
+// use user input to update state 
+// update DOM to reflect the new state
+selectEl.addEventListener('change', (e) => {
+    console.log('user clicked');
+});
---
+++ data.js
@@ -0,0 +1,16 @@
+export const people = [{
+    id: 1,
+    name: "Sydnie",
+    age: 20,
+    bio: 'Studying to become a full stack developer,enjoys fun, vacations, and new experiences.'
+    hobbies: ['reading', 'programming', 'eating']
+},
+{
+    id: 2,
+    name: "Joel",
+    age: 26,
+    bio: 'Owner of McCandless Insurance Group. Ex-professional football player and lover of helping others',
+    hobbies: ['walking dog', 'reading', 'traveling']
+
+},
+];
